0aa60850b29c8a032c7a0ec422a49fb6491a61cc
Priyanshu Gupta <pgupta962000@gmail.com>
2023-06-12 14:50:10 +0530

    modify react app

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import './App.css';
-
+import Post from "./Post";
+import Header from './Header';
+import {Route, Routes} from "react-router-dom";
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 function App() {
   return (
-     <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-      <div className="image">
-        <img src="https://avatars.githubusercontent.com/u/96141079?v=4" alt="" />
-      </div>
-        <div className="texts">
-        <h2>jbvhbvjbs,bjsnbbxc,bb vbhbvxcb</h2>
-        <p className="info">
-          <a className="author">Priyanshu Gupta</a>
-          <time>2023-06-12 10:16</time>
-        </p>
-        <p className="summary">jbhbhxbhjxbhdbhzxbchbhjghd fbb</p>
-        </div>
-      </div>
-      <div className="post">
-      <div className="image">
-        <img src="https://en.wikipedia.org/static/images/mobile/copyright/wikipedia-wordmark-en.svg" alt="" />
-        </div>
-        <div className="texts">
-        <h2>jbvhbvjbs,bjsnbbxc,bb vbhbvxcb</h2>
-        <p className="info">
-          <a className="author">Priyanshu Gupta</a>
-          <time>2023-06-12 10:16</time>
-        </p>
-        <p className="summary">jbhbhxbhjxbhdbhzxbchbhjghd fbb</p>
-        </div>
-      </div>
-      <div className="post">
-      <div className="image">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/225px-Instagram_logo_2022.svg.png" alt="" />
-        </div>
-        <div className="texts">
-        <h2>jbvhbvjbs,bjsnbbxc,bb vbhbvxcb</h2>
-        <p className="info">
-          <a className="author">Priyanshu Gupta</a>
-          <time>2023-06-12 10:16</time>
-        </p>
-        <p className="summary">jbhbhxbhjxbhdbhzxbchbhjghd fbb</p>
-        </div>
-      </div>
-     </main>
+     <Routes>
+     <Route path="/" element={<Layout />}>
+     <Route index element={<IndexPage />} />
+      <Route path={'/login'} element={<LoginPage />} />
+<Route path={'/register'} element={<RegisterPage />} />
+     </Route>
+     </Routes>
+     
   );
 }
 
