19d246b4312e2cf8a46d8574105e23967ac8954f
Priyanshu Gupta <pgupta962000@gmail.com>
2023-06-12 10:35:20 +0530

    update react app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,45 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+      <div className="image">
+        <img src="https://avatars.githubusercontent.com/u/96141079?v=4" alt="" />
+      </div>
+        <div className="texts">
+        <h2>jbvhbvjbs,bjsnbbxc,bb vbhbvxcb</h2>
+        <p className="info">
+          <a className="author">Priyanshu Gupta</a>
+          <time>2023-06-12 10:16</time>
+        </p>
+        <p className="summary">jbhbhxbhjxbhdbhzxbchbhjghd fbb</p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img src="https://en.wikipedia.org/static/images/mobile/copyright/wikipedia-wordmark-en.svg" alt="" />
+        </div>
+        <div className="texts">
+        <h2>jbvhbvjbs,bjsnbbxc,bb vbhbvxcb</h2>
+        <p className="info">
+          <a className="author">Priyanshu Gupta</a>
+          <time>2023-06-12 10:16</time>
+        </p>
+        <p className="summary">jbhbhxbhjxbhdbhzxbchbhjghd fbb</p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/225px-Instagram_logo_2022.svg.png" alt="" />
+        </div>
+        <div className="texts">
+        <h2>jbvhbvjbs,bjsnbbxc,bb vbhbvxcb</h2>
+        <p className="info">
+          <a className="author">Priyanshu Gupta</a>
+          <time>2023-06-12 10:16</time>
+        </p>
+        <p className="summary">jbhbhxbhjxbhdbhzxbchbhjghd fbb</p>
+        </div>
+      </div>
      </main>
   );
 }
