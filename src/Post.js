84296edc95371341334258aeb5ab31dcf12af064
Priyanshu Gupta <pgupta962000@gmail.com>
2023-06-12 14:57:39 +0530

    create separate post file

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://avatars.githubusercontent.com/u/96141079?v=4" alt="" />
+            </div>
+        <div className="texts">
+            <h2>jbvhbvjbs,bjsnbbxc,bb vbhbvxcb</h2>
+            <p className="info">
+            <a className="author">Priyanshu Gupta</a>
+            <time>2023-06-12 10:16</time>
+            </p>
+            <p className="summary">jbhbhxbhjxbhdbhzxbchbhjghd fbb</p>
+        </div>
+        </div>
+    );
+}
