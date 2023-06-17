a97848dce025641ccfc42e86dd56af58ed76c590
Priyanshu Gupta <pgupta962000@gmail.com>
2023-06-17 18:57:43 +0530

    modify IndexPage with setPosts

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,10 +1,20 @@
 import Post from "../Post"
+import { useEffect, useState } from "react";
+//import { response } from "express";
 export default function IndexPage(){
+    const [posts,setPosts] = useState([]);
+    useEffect(() =>{
+        fetch('http://localhost:4000/post').then(response => {
+            response.json().then(posts =>{
+                setPosts(posts);
+            });
+        });
+    },[]);
     return(
-        <div>
-            <Post />
-            <Post />
-            <Post />
-        </div>
+        <>
+            {posts.length > 0 && posts.map(post =>(
+                <Post {...post} />
+            ))}
+        </>
     ) ;
 }
